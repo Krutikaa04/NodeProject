bf60b36b4e61051a16e0c93037510d8ba0d67a68
Krutikaa04 <waghkrutika01@gmail.com>
2023-09-18 11:14:33 +0530

    files updated

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res)=> {
     res.send("Welcome to express");
 })
+
+// app.post("/", (req, res)=>{
+//     console.log("request--", req)
+//     res.send("Hello World!")
+// })
 
 app.listen(port, ()=>{
     console.log(`App running on port ${port}`);
