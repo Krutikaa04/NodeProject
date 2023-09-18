95f7ef69658a87ead25111eba185370ee453f6bc
Krutikaa04 <waghkrutika01@gmail.com>
2023-09-18 16:26:33 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res)=> {
+    console.log("get request --", req.body)
     res.send("Welcome to express");
 })
 
 // app.post("/", (req, res)=>{
-//     console.log("request--", req)
+//     console.log("request--", req.body)
 //     res.send("Hello World!")
+// })
+
+// app.put("/", (req, res)=>{
+//     console.log("put reqest--", req.body)
+//     res.send("hello from put request")
 // })
 
 app.listen(port, ()=>{
